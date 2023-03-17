172a6fb0b0f2d98a98facee75213dfc2e95c9546
이준용 <iiiuooyoo@gmail.com>
2023-03-17 18:00:31 +0900

    gap 남음

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import styled from "@emotion/styled";
 import { randomEmojis } from "./utils/randomEmojis";
 
@@ -31,6 +31,18 @@ function Intro() {
         setVibrate((vibrate) => !vibrate);
     }
 
+    function handleGapChange(event: ChangeEvent<HTMLInputElement>) {
+        const { target } = event;
+        const { name, value } = target;
+
+        setGaps((gaps) => ({
+            ...gaps,
+            [name]: parseInt(value),
+        }));
+
+        console.log(name, value);
+    }
+
     useEffect(() => {
         if (vibrate) {
             setTimeout(() => {
@@ -40,27 +52,48 @@ function Intro() {
     }, [vibrate]);
 
     return (
-        <div className="flex flex-col bgColor">
+        <div className="min-h-[100vh] flex flex-col bgColor">
             {/* <h1>미안하다 이거 보여주려고 어그로끌었다</h1> */}
             <div className="text-center text-white pt-10 text-4xl font-semibold ">
                 CSS Grid Viewer
             </div>
             <section className="flex justify-between p-10 grid-container">
                 <MainStyled
-                    className={`w-[60%] h-[500px] grid grid-main gap-x-[${
+                    className={`w-[60%] h-[500px] grid grid-main gap-x-${
                         gaps.row
-                    }px] gap-y-[${gaps.column}px] ${vibrate ? "vibrate" : ""}`}
+                    } gap-y-${gaps.column} ${vibrate ? "vibrate" : ""}`}
                 >
-                    {emojis.map((emoji) => (
-                        <div className="text-center text-4xl grid place-items-center tossface emoji-box">
-                            {emoji}
+                    {emojis.map((emoji, index) => (
+                        <div
+                            className="text-center text-4xl grid place-items-center tossface emoji-box"
+                            key={index}
+                        >
+                            <span>{emoji}</span>
                         </div>
                     ))}
                 </MainStyled>
-                <AsideStyled className="w-[30%]">
+                <AsideStyled className="w-[30%] flex flex-col gap-y-4 grid-aside">
                     <button className="btn-basic" onClick={emojiReload}>
                         Emoji reload
                     </button>
+                    <label>
+                        Row Gap
+                        <input
+                            type="number"
+                            min={0}
+                            name="row"
+                            onChange={handleGapChange}
+                        />
+                    </label>
+                    <label>
+                        Column Gap
+                        <input
+                            type="number"
+                            min={0}
+                            name="column"
+                            onChange={handleGapChange}
+                        />
+                    </label>
                 </AsideStyled>
             </section>
         </div>
